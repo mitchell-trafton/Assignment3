a1846fd64982408c63bd513b6a9e8dbc5a794ec3
Mitchell <mastrafton@gmail.com>
2021-02-25 18:15:20 -0600

    Updated output font

--- Assignment3/Form1.Designer.cs
+++ Assignment3/Form1.Designer.cs
@@ -510,6 +510,7 @@ namespace Assignment3
             // 
             // query_txt
             // 
+            this.query_txt.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.query_txt.Location = new System.Drawing.Point(545, 43);
             this.query_txt.Multiline = true;
             this.query_txt.Name = "query_txt";
